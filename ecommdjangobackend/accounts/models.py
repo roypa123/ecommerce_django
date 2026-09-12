a17249cc4c4aa9742315fee9ec7ff9d31ea33988
roypa123 <roypa81130@gmail.com>
2026-09-12 18:31:01 +0530

    fix: some changes

--- ecommdjangobackend/accounts/models.py
+++ ecommdjangobackend/accounts/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
+
+from .managers import CustomUserManager
 # Create your models here.
+
+class User(AbstractBaseUser):
+    username = None
+    name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
--- ecommdjangobackend/app/settings.py
+++ ecommdjangobackend/app/settings.py
@@ -119,6 +119,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
