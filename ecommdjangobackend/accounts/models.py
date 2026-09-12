271863464a249ed01f43ff2afb71258029c9ec83
roypa123 <roypa81130@gmail.com>
2026-09-12 18:45:26 +0530

    fix: some changes

--- ecommdjangobackend/accounts/models.py
+++ ecommdjangobackend/accounts/models.py
@@ -1,17 +1,17 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 from .managers import CustomUserManager
 # Create your models here.
 
-class User(AbstractBaseUser):
+class User(AbstractUser):
     username = None
     name = models.CharField(max_length=150)
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['name']
 
     objects = CustomUserManager()
 
