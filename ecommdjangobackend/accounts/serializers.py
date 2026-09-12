823dfcf14a65b7c68267629cb8d97ac174104a63
roypa123 <roypa81130@gmail.com>
2026-09-12 22:11:45 +0530

    fix: some changes

--- ecommdjangobackend/accounts/serializers.py
+++ ecommdjangobackend/accounts/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
@@ -13,3 +13,20 @@ class SignupSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        user = authenticate(
+            request=self.context.get('request'),
+            username=attrs['email'],
+            password=attrs['passwrod'],
+        )
+        if not user:
+            raise serializers.ValidationError('Invalid email or password')
+        attrs['user'] = user
+        return attrs
+
+         
--- ecommdjangobackend/accounts/views.py
+++ ecommdjangobackend/accounts/views.py
@@ -1,9 +1,10 @@
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
-from .serializers import SignupSerializer
+from .serializers import SignupSerializer, LoginSerializer
 
 # Create your views here.
 class SignupView(generics.CreateAPIView):
@@ -23,3 +24,22 @@ class SignupView(generics.CreateAPIView):
             'access': str(refresh.access_token),
             'refresh': str(refresh),
         }, status=status.HTTP_201_CREATED)
+
+
+class LoginView(APIView):
+
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = LoginSerializer(data=request.data, context={'request':request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'name': user.name,
+            'email': user.email,
+            'access': str(refresh.access_token),
+            'refresh': str(refresh),
+        }, status=status.HTTP_200_CREATED)
