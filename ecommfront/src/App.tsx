8efad7cbe4eda99f5090b95128653a959984f462
roypa123 <roypa81130@gmail.com>
2026-09-13 12:14:39 +0530

    fix: some changes

--- ecommfront/src/App.tsx
+++ ecommfront/src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <>
+    <TooltipProvider>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+      <Button onClick={() => alert("Clicked!")}>Click me</Button>
+    </TooltipProvider>
   );
 }
 
