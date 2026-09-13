7a2d927857f99ba654c36161eca3cbd22ebd6690
roypa123 <roypa81130@gmail.com>
2026-09-13 20:00:34 +0530

    fix: some changes

--- ecommfront/src/main.tsx
+++ ecommfront/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import './index.css'
 import App from './App.tsx'
-import AppRouter from './routing/AppRouter.tsx'
+
 
 
 
--- ecommfront/src/routing/AppRouter.tsx
+++ ecommfront/src/routing/AppRouter.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignupPage from "@/pages/SignupPage";
+// import HomePage from "@/pages/HomePage";
 import App from "@/App";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<div>Hello</div>} />
         <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
