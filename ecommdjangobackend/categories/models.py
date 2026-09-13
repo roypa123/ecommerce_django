bbcbd6265f8d43b2f0472784bbb96859fdd1b68b
roypa123 <roypa81130@gmail.com>
2026-09-13 09:47:23 +0530

    fix: some changes

--- ecommdjangobackend/categories/models.py
+++ ecommdjangobackend/categories/models.py
@@ -7,4 +7,19 @@ class Category(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    
+    def __str__(self):
+        return self.name
+
+
+class Subcategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="subcategories")
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='subcategories')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('category', 'name')
+
+    def __str__(self):
+        return f"{self.category.name} > {self.name}"
