565818b72d1f085652f2a0a1365ff45601204977
roypa123 <roypa81130@gmail.com>
2026-09-14 15:04:17 +0530

    fix: some changes

---
+++ ecommfront/src/endpoints/category.ts
@@ -0,0 +1,31 @@
+import { apiclient } from "@/api/client";
+import type {
+    Category,
+    Subcategory,
+    CreateCategoryPayload,
+    CreateSubcategoryPayload,
+} from "@/types/category"
+
+export const getCategories = async (): Promise<Category[]> => {
+    const { data } = await apiclient.get<Category[]>("/categories/");
+    return data;
+};
+
+export const createCategoryRequest = async (
+    payload: CreateCategoryPayload
+): Promise<Category> => {
+    const formData = new FormData();
+    formData.append("name", payload.name);
+    formData.append("image", payload.image);
+
+    const { data } = await apiclient.post<Category>("/categories/", formData);
+    return data;
+
+}
+
+
+export const createSubcategoryRequest = async (
+    payload: CreateSubcategoryPayload
+): Promise<Subcategory> => {
+    
+}
---
+++ ecommfront/src/types/category.ts
@@ -0,0 +1,29 @@
+export interface Subcategory {
+    id: number;
+    category: number;
+    name: string;
+    image: string;
+    created_at: string;
+    updated_at: string;
+}
+
+export interface Category {
+    id: number;
+    name: string;
+    image: string;
+    subcategories: Subcategory[];
+    created_at: string;
+    updated_at: string;
+}
+
+
+export interface CreateCategoryPayload {
+    name: string;
+    image: File;
+}
+
+export interface CreateCategoryPayload {
+    category: number;
+    name: string;
+    image:File
+}
